2ec49272ca45d77b42690efec6a1ad6f9e15de8b
Alfredo Casanova <maattos07@outlook.com>
2023-10-09 22:46:11 -0500

    Agregar archivos de proyecto.

---
+++ 24CV WEB/Controllers/EjemplosController.cs
@@ -0,0 +1,22 @@
+﻿using _24CV_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _24CV_WEB.Controllers
+{
+    public class EjemplosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Alfredo";
+            persona.Apellido = "Casanova";
+            persona.FechaNacimiento = DateTime.Now;
+            return View(persona);
+        }
+    }
+}
